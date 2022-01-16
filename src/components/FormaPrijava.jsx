579fff7a16cbde28b007c4e10b791108ed9a77d0
UrosJovanovic9 <urosh.jovanovicc@gmail.com>
2022-01-16 23:45:13 +0100

    Dodata forma za prijavu

---
+++ src/components/FormaPrijava.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Forma from "./Forma.jsx";
+
+function FormaPrijava() {
+    return (
+        <div>
+
+            <Forma  prijava = {"da"}/>
+            
+        </div>
+    )
+}
+
+export default FormaPrijava
