59603f660ed25e6dc2e02b7c03ca395088dcbf61
UrosJovanovic9 <urosh.jovanovicc@gmail.com>
2022-01-16 23:45:31 +0100

    Dodata forma za registraciju

---
+++ src/components/FormaRegistracija.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Forma from "./Forma.jsx";
+function FormaRegistracija() {
+    return (
+        <div>
+            <Forma prijava = {"ne"}/>
+        </div>
+    )
+}
+
+export default FormaRegistracija
