83bfd58a6c154cc5ec56abb922464fdba6ed7eb0
UrosJovanovic9 <urosh.jovanovicc@gmail.com>
2022-01-19 11:08:20 +0100

    Dodata komponenta za jedan film

--- src/components/JedanFilm.jsx
+++ src/components/JedanFilm.jsx
@@ -1,35 +1,82 @@
 import React from 'react'
 import HarryPotterDvoranaTajni from '../img/HarryPotterDvoranaTajni.jpg'
-import { BsHeart, BsCheck2, BsCheckLg} from "react-icons/bs";
+import { BsHeart, BsCheck2, BsCheckLg,BsEye} from "react-icons/bs";
+import { useState } from "react";
 
-function JedanFilm() {
+function JedanFilm({film, prva, povecajZaJedan}) {
+    // console.log(props);
+    
+    const[brojSvidjanja, setBrojSvidjanja] = useState(0);
+    
+    function povecaj(){
+      setBrojSvidjanja(brojSvidjanja+1);
+    }
+
     return (
-        <div className = "jedanFilm" hover-zoom>
-            <img
+        <div className = "jedanFilm">
+
+        {prva === "jeste" ? (
+        <>
+        <img
         className="slika"
-        src={HarryPotterDvoranaTajni}
-        
-         alt="Ovo je opis filma koji mozete videti"
+        src={film.slika}
       />
-      <div class="centered">Pogledaj film</div>
+      <div className="centered">Pogledaj film</div>
 
       <div className="jedanFilmTekst">
-        <h3 className="jedanFilmNaslov">Hari Poter i dvorana tajni</h3>
-        <p className="jedanFilmOpis">2002 | avanturisticki</p>
+        <p className="jedanFilmNaslov">{film.naziv}</p>
+        <p className="jedanFilmOpis">{film.opis}</p>
       </div>
 
 
-      <button className="btn1">
+      <button className="btn3" onClick = {() => povecajZaJedan(film.id)}> 
+         <BsEye /> 
+       </button>
+
+      <button className="btn1" onClick={povecaj} >
         <BsHeart />
        </button>
 
       <button className="btn2" alt= "Ovo je dugme"> 
          <BsCheckLg /> 
        </button>
+       
 
-       <p>7.4</p>
+       <p>{brojSvidjanja}</p> 
+       </>) : (<>
+
+       
+        
+       <table className="tabela">
+        <thead>
+          <tr>
+            <th>Naziv filma</th>
+            <th>Ocena</th>
+            <th>Pregledi</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+          <tr>
+            <td>{film.naziv}</td>
+            <td>{film.ocena}</td>
+            <td>{film.pregled}</td>
+          </tr>
+
+        </tbody>
+        </table>
+        
+
+        {/* <h2>Naziv: {film.naziv}</h2> */}
+       {/* <h3>Ocena: {film.ocena}</h3> */}
+       {/* <h3>Pregled: {film.pregled}</h3> */}
+       
+
+
+       </>)}
+     
       
-        </div>
+      </div>
     )
 }
 
