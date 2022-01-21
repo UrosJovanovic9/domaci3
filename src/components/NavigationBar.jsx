3ad49f5934fbc6371442f641dee5ad4a68a63a11
UrosJovanovic9 <urosh.jovanovicc@gmail.com>
2022-01-21 19:42:16 +0100

    Izmenjen navigation bar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { SiThemoviedatabase } from "react-icons/si";
+import { GiDramaMasks } from "react-icons/gi";
+import { MdTheaterComedy } from "react-icons/md";
+import { BiCameraMovie } from "react-icons/bi";
+import { BsSearch } from "react-icons/bs";
+import {Link } from "react-router-dom";
+
 
 function NavigationBar() {
     return (
        
         <div className='navigationBar'>
+           
             <div className = 'logo'>
                 <SiThemoviedatabase/>
             </div>
-            <a>Najgledaniji filmovi</a>
+            <Link to = "/" className = "najgledaniji">Najgledaniji filmovi</Link>
+            <Link to = "/pregled"className = "pregled">Pregled filmova</Link>
+            <Link to = "/prijava"className = "prijava">Prijava</Link>
+            <Link to = "/registracija"className = "registracija">Registracija</Link>
+             <BsSearch className = "search"/>
+
+            
         </div>
     )
 }
