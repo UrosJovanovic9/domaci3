a561e180e861acdea103a9447113121a34eab4e6
UrosJovanovic9 <urosh.jovanovicc@gmail.com>
2022-01-25 20:54:57 +0100

    Dodata funkcionalnost za prikazivanje opisa filma

--- src/components/JedanFilm.jsx
+++ src/components/JedanFilm.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HarryPotterDvoranaTajni from '../img/HarryPotterDvoranaTajni.jpg'
 import { BsHeart, BsCheck2, BsCheckLg,BsEye} from "react-icons/bs";
+import { FiMoreHorizontal} from "react-icons/fi";
 import { useState } from "react";
 
 function JedanFilm({film, prva, povecajZaJedan}) {
@@ -12,15 +13,31 @@ function JedanFilm({film, prva, povecajZaJedan}) {
       setBrojSvidjanja(brojSvidjanja+1);
     }
 
+    const[opisFilma, setOpisFilma] = useState("");
+    const[i, seti] = useState(0);
+    function prikaziOpis(){
+        if(i % 2 === 0){
+          setOpisFilma(film.opisFilma);
+        }else{
+          setOpisFilma("");
+        }
+        console.log(i);
+       seti(i+1);
+    }
+    
+
     return (
         <div className = "jedanFilm">
 
         {prva === "jeste" ? (
         <>
+        <div>
         <img
         className="slika"
         src={film.slika}
       />
+      <div className = "opisFilma">{opisFilma}</div>
+      </div>
       <div className="centered">Pogledaj film</div>
 
       <div className="jedanFilmTekst">
@@ -37,25 +54,23 @@ function JedanFilm({film, prva, povecajZaJedan}) {
         <BsHeart />
        </button>
 
-      <button className="btn2" alt= "Ovo je dugme"> 
-         <BsCheckLg /> 
+      <button className="btn2" onClick={prikaziOpis}> 
+         <FiMoreHorizontal /> 
        </button>
        
 
        <p>{brojSvidjanja}</p> 
        </>) : (<>
-
-       
         
        <table className="tabela">
-        <thead>
-          <tr>
+        {/* <thead> } */}
+          {/* <tr>
             <th>Naziv filma</th>
             <th>Ocena</th>
             <th>Pregledi</th>
-          </tr>
-        </thead>
-        <tbody>
+          </tr> */}
+        {/* { </thead> */}
+        {/* <tbody>
           
           <tr>
             <td>{film.naziv}</td>
@@ -63,9 +78,19 @@ function JedanFilm({film, prva, povecajZaJedan}) {
             <td>{film.pregled}</td>
           </tr>
 
-        </tbody>
+        </tbody> */}
         </table>
         
+
+        {/* <tbody>
+        {products.map(product => (
+          <tr key={product.id}>
+            <td>{product.name}</td>
+            <td>{product.price}</td>
+            <td>{product.stock}</td>
+          </tr>
+        ))}
+      </tbody> */}
 
         {/* <h2>Naziv: {film.naziv}</h2> */}
        {/* <h3>Ocena: {film.ocena}</h3> */}
